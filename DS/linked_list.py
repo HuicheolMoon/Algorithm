f58d6c0905d3da799ecdd43b42265b82b6e3a5bc
Huicheol Moon <huicheol7@gmail.com>
2021-05-12 23:49:18 +0900

    0512

---
+++ DS/linked_list.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def append_node(self, node):
+        if self.length == 0:
+            self.head = node
+        else:
+            tail = self.head
+            while tail.next:
+                tail = tail.next
+            tail.next = node
+        self.length += 1
+
+    def delete_node(self, idx):
+        if idx >= self.length:
+            raise IndexError("인덱스가 리스트 범위 밖입니다.")
+        elif idx == 0:
+            self.head = self.head.next
+        else:
+            count = 1
+            front = self.head
+            ghost = self.head.next
+            while count < idx:
+                front = ghost
+                ghost = front.next
+                count += 1
+            front.next = ghost.next
+        self.length -= 1
+
+    def insert_node(self, node, idx):
+        if idx > self.length:
+            raise IndexError("인덱스가 리스트 범위 밖입니다.")
+        elif idx == 0:
+            node.next = self.head
+            self.head = node
+        else:
+            count = 1
+            front = self.head
+            while count < idx:
+                front = front.next
+                count += 1
+            node.next = front.next
+            front.next = node
+        self.length += 1
+
+    def get_index(self, value):
+        if self.length == 0:
+            raise IndexError("해당하는 값이 없습니다.")
+        tail = self.head
+        idx = 0
+        while tail:
+            if tail.value == value:
+                return idx
+            tail = tail.next
+            idx += 1
+        raise IndexError("해당하는 값이 없습니다.")
+
+
+if __name__ == "__main__":
+    x1 = LinkedList()
+    x1.append_node(Node(4))
+    x1.append_node(Node(2))
+    x1.append_node(Node(3))
+    x1.append_node(Node(1))
+    x1.insert_node(Node(5), 2)
+    print(x1.get_index(2))
+    print(x1.get_index(3))
+    x1.insert_node(Node(6), 2)
+    x1.delete_node(3)
+    print(x1.get_index(2))
---
+++ DS/stack_of_linked_list.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def push(self, value):
+        node = Node(value)
+        if self.length == 0:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            raise IndexError("빈 스택입니다.")
+        elif self.length == 1:
+            result = self.tail.value
+            self.head = None
+            self.tail = None
+        else:
+            new_tail = self.head
+            for _ in range(self.length - 2):
+                new_tail = new_tail.next
+            result = self.tail.value
+            new_tail.next = None
+        self.length -= 1
+        print(result)
+        return result
+
+    def peek(self):
+        if self.length == 0:
+            raise IndexError("빈 스택입니다.")
+        else:
+            result = self.tail.value
+            print(result)
+            return result
+
+    def is_empty(self):
+        result = True if self.length == 0 else False
+        print(result)
+        return result
+
+
+if __name__ == "__main__":
+    x1 = Stack()
+    x1.push(4)
+    x1.push(2)
+    x1.push(3)
+    x1.push(1)
+    x1.pop()
+    x1.peek()
+    x1.pop()
+    x1.push(5)
+    x1.peek()
+    x1.is_empty()
