6b4fdd55858c84da0bd91b5ef7f61e65466af3d9
Huicheol Moon <huicheol7@gmail.com>
2021-10-03 07:45:22 +0900

    programmers 21946

---
+++ programmers/21946.py
@@ -0,0 +1,23 @@
+# Problem : https://programmers.co.kr/learn/courses/30/lessons/12946
+# Date : 2021-10-03
+# 하노이의 탑
+
+def solution(n):
+    dp = [[]] * n
+    dp[0] = [[1, 3]]
+    for i in range(1, n):
+        dp[i] = trans(dp[i-1], "front") + dp[0] + trans(dp[i-1], "back")
+    answer = dp[n-1]
+    return answer
+
+
+def trans(arr, mode):
+    trans_dict = dict()
+    if mode == "front":
+        trans_dict = {1: 1, 2: 3, 3: 2}
+    elif mode == "back":
+        trans_dict = {1: 2, 2: 1, 3: 3}
+    else:
+        raise Exception("mode error")
+    result = [[trans_dict[y] for y in x] for x in arr]
+    return result
