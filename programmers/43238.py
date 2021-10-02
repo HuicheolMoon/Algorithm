cc93749a1262605c688d9d2452df389d1db682b3
Huicheol Moon <huicheol7@gmail.com>
2021-10-03 07:32:37 +0900

    update

---
+++ programmers/43238.py
@@ -0,0 +1,18 @@
+# Problem : https://programmers.co.kr/learn/courses/30/lessons/43238
+# Date : 2021-10-03
+# 입국심사
+
+def solution(n, times):
+    low = (n // len(times)) * min(times)
+    high = (n // len(times)) * max(times)
+    while low < high:
+        people = 0
+        mid = (low + high) // 2
+        for officer in times:
+            people += (mid // officer)
+        if people < n:
+            low = mid + 1
+        else:
+            high = mid
+    answer = low
+    return answer
---
+++ programmers/49189.py
@@ -0,0 +1,26 @@
+# Problem : https://programmers.co.kr/learn/courses/30/lessons/49189
+# Date : 2021-10-03
+# 가장 먼 노드
+
+from collections import defaultdict
+
+def solution(n, edge):
+    vertex = defaultdict(list)
+    discover = [False for _ in range(n+1)]
+    distances = [0] + [float("inf") for _ in range(n)]
+    for line in edge:
+        vertex[line[0]].append(line[1])
+        vertex[line[1]].append(line[0])
+    queue = []
+    queue.append([1, 0])
+    while queue:
+        start = queue.pop(0)
+        if start[1] < distances[start[0]]:
+            distances[start[0]] = start[1]
+        for dest in vertex[start[0]]:
+            if not discover[dest]:
+                discover[dest] = True
+                end = [dest, start[1] + 1]
+                queue.append(end)
+    answer = distances.count(max(distances))
+    return answer
