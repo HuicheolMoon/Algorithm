c23a334b65aa036bac76293eecabfbdc987aa441
Huicheol Moon <huicheol7@gmail.com>
2021-11-14 16:03:47 +0900

    programmers 84512

--- Programmers/84512.py
+++ Programmers/84512.py
@@ -4,13 +4,11 @@
 
 def solution(word):
     word_dict = ["A", "E", "I", "O", "U"]
-    new_word = word + " " * (len(word_dict) - len(word))
     pivot = 0
+    answer = 0
     for _ in range(len(word_dict)):
         pivot = 5 * pivot + 1
-    answer = pivot * word_dict.index(new_word[0]) + 1
-    pivot = (pivot - 1) // 5
-    for char in word[1:]:
+    for char in word:
         if char == " ":
             return answer
         index = word_dict.index(char)
