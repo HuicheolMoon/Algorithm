d55832ea8fe1673c4debcfc36767235f4a173576
Huicheol Moon <huicheol7@gmail.com>
2021-12-28 01:29:40 +0900

    leetcode 1476

---
+++ LeetCode/1476. Subrectangle Queries.py
@@ -0,0 +1,22 @@
+# Problem : https://leetcode.com/problems/subrectangle-queries/
+# Date : 2021-12-28
+
+
+class SubrectangleQueries:
+
+    def __init__(self, rectangle: List[List[int]]):
+        self.rectangle = rectangle
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for y in range(row1, row2 + 1):
+            self.rectangle[y] = self.rectangle[y][:col1] + ([newValue] * (col2 - col1 + 1)) + self.rectangle[y][
+                                                                                              col2 + 1:]
+
+    def getValue(self, row: int, col: int) -> int:
+        return self.rectangle[row][col]
+
+
+# Your SubrectangleQueries object will be instantiated and called as such:
+# obj = SubrectangleQueries(rectangle)
+# obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+# param_2 = obj.getValue(row,col)
