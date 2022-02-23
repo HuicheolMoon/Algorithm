5cebdd20b7d09b71eced88b4cec67835e842ea1b
HuicheolMoon <huicheol7@gmail.com>
2022-02-23 17:22:37 +0900

    Update dfs

--- algorithm_code/dfs.py
+++ algorithm_code/dfs.py
@@ -1,34 +1,35 @@
 # DFS
 
-from collections import defaultdict
 
-graph = defaultdict(list)
-# graph[x].append(y) for edge in edges
-# or graph = defaultdict(dict); graph[x][y] = z:cost
+# v: # of vertices
+# e: # of edges
+# graph: 2D array for distances
+# graph = [[distance for _ in range(v)] for __ in range(v)]]
 
 
-def dfs(graph, root):
-    # if unknown N(=len(nodes)), visited = [] & visited.append()
-    visited = [False for _ in range(N)]
-    stack = [root] # stack = [[root, obj(root)]] for obj funcstion & value
-
-# Obj function ----------
-
+# 1. Using stack
+def dfs(root):
+    stack = [root]
+    visited = [False for _ in range(v)]
     while stack:
-        start = stack.pop()
-        if not visited[start]:
-            visited[start] = True
-            for dest in graph[start]:
-                if not visited[dest]:
-                    stack.append(dest)
+        start = stack[-1]
+        for i in range(v):
+            if graph[start][i] > 0 and not visited[i]:
+                visited[i] = True
+                stack.append(i)
+                break
+        else:
+            stack.pop()
+    return
 
-# Adjust Obj function value ----------
 
-    return #target
+# 2. Recursion
+visited = [False for _ in range(v)]
 
 
-'''
-- 두 정점 연결 여부 : 한 정점 root로 하고 dfs() 후 visited[other] (양방향 그래프의 단방향 연결 시 정점 스위칭)
-- 독립 부분그래프 개수 : while (False in visited): root를 visited.index(False)로 반복한 횟수
-- 방문 순서 : obj_list에 obj_list.append(start) 반복
-'''
+def dfs(start):
+    visited[start] = True
+    for i in range(v):
+        if graph[start][i] > 0 and not visited[i]:
+            dfs(i)
+    return
