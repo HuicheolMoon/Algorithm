693f3fa23944299c70ca6b65b8dacd202d4f1eb1
HuicheolMoon <huicheol7@gmail.com>
2022-02-24 11:34:39 +0900

    Update bfs

--- algorithm_code/bfs.py
+++ algorithm_code/bfs.py
@@ -1,36 +1,24 @@
 # BFS
 
-from collections import defaultdict, deque
 
-graph = defaultdict(list)
-# graph[x].append(y) for edge in edges
-# edge == [x: start, y: dest, (z: cost)]
+# v: # of vertices
+# e: # of edges
+# graph: 2D array for distances
+# graph = [[distance for _ in range(v)] for __ in range(v)]]
 
 
-def bfs(graph, root):
-    # if dont know N(=len(nodes)), visited = [] & visited.append()
-    visited = [[False, 0] for _ in range(N)]    # or [False, []]
-    visited[root] = [True, 0]   # or [True, [root]]
+# 1. Using queue
+from collections import deque
+
+
+def bfs(root):
+    visited = [False for _ in range(v)]
+    visited[root] = True
     queue = deque([root])
-
-# Obj function ----------
-
     while queue:
         start = queue.popleft()
-        for dest in graph[start]:
-            if not visited[dest][0]:
-                visited[dest][0] = True
-                queue.append(dest)
-                visited[dest][1] = visited[start][1] + 1    # 새 정점을 방문하는 것이 항상 최단 경로
-                # or visited[dest][1] = visited[start][1][:]
-                # visited[dest][1].append(dest)
-
-# Adjust Obj function value ----------
-
-    return # target
-
-
-'''
-- 최단 경로 : obj : list일 때, [x[1] for x in visited]
-- 최단 경로의 길이 : obj : int일 때, [x[1] for x in visited]
-'''
+        for i in range(v):
+            if graph[start][i] > 0 and not visited[i]:
+                visited[i] = True
+                queue.append(i)
+    return
