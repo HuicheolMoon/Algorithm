d410c9fab2539f68e3b6b627b9a1eac9e6a098cc
HuicheolMoon <huicheol7@gmail.com>
2022-04-04 14:33:10 +0900

    Update bellman-ford

--- algorithm_code/shortest_path/Bellman-Ford.py
+++ algorithm_code/shortest_path/Bellman-Ford.py
@@ -1,25 +1,27 @@
 # Bellman-Ford: 두 정점간의 최단거리 - O(EV)
+# 모든 edge에 대해서 도착점을 제외한 v-1개의 정점에 대해 최단경로 갱신을 반복
 
-from collections import defaultdict
 
-# graph = defaultdict(dict) -> graph[x][y] = cost
-
+# graph = [[{distance} for _ in range(v)] for __ in range(v)] : 그래프의 정보를 저장한 인접행렬
+# v = (# of vertices)
+# e = (# of edges)
 
 def bellman_ford(graph, root):
-    distances = {node: float("inf") for node in graph}  # root로부터의 거리
-    distances[root] = 0
-
-    for _ in range(len(graph) - 1):
-        for start, x in graph.items():
-            for dest, cost in x.items():
-                dist = distances[start] + cost
-                if distances[dest] > dist:
-                    distances[dest] = dist
-
-    for node in graph:
-        for neighbor, cost in graph[node].items():
-            dist = distances[node] + cost
-            if distances[neighbor] > dist:
+    distance_list = [float("inf") for _ in range(v)]  # root로부터의 거리
+    distance_list[root] = 0
+    # 알고리즘 작동 단계
+    for _ in range(v-1):    # V
+        for s in range(v):  # E (모든 vertices pair 중 edge가 연결된 pair만 선택하여 연산하므로)
+            for f in range(v):
+                if graph[s][f] < INF:
+                    new_distance = distance_list[s] + graph[s][f]
+                    if new_distance < distance_list[f]:
+                        distance_list[f] = new_distance
+    # 음수 사이클 검증 단계
+    for s in range(v):
+        for f in range(v):
+            new_distance = distance_list[s] + graph[s][f]
+            if new_distance < distance_list[f]:
+                # 모든 정점에 대해 최단경로를 갱신해도 다른 최단경로가 존재 == 음수 사이클이 존재 -> 최단거리가 존재하지 않음
                 return "Failure"
-
-    return distances
+    return distance_list
